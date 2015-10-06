4e1226d9c330ded89718842987640bea437a71e8
HSHERU <nrgrs@hotmail.com>
2015-10-06 17:09:22 +0300

    test

--- app/src/main/java/com/example/asus/quraan/Athkar.java
+++ app/src/main/java/com/example/asus/quraan/Athkar.java
@@ -1,39 +1,23 @@
 package com.example.asus.quraan;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-
-public class Browse extends AppCompatActivity {
+public class Athkar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browse);
+        setContentView(R.layout.activity_athkar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_browse, menu);
+        getMenuInflater().inflate(R.menu.menu_athkar, menu);
         return true;
-    }
-    public void HolyQuran(View view){
-        Intent HolyQuranIntent = new Intent(this,Quran_Prints.class);
-        startActivity(HolyQuranIntent);
-    }
-    public void Athkar(View view){
-        Intent AthkarIntent = new Intent(this,Athkar.class);
-        startActivity(AthkarIntent);
-    }
-    public void Tajweed(View view){
-        Intent TajweedIntent = new Intent(this,Tajweed.class);
-        startActivity(TajweedIntent);
     }
 
     @Override
